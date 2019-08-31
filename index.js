9051dc6d742d3f8a94b5e7be3db001c3eba47e30
your name here <your_email_here>
2019-08-30 23:35:35 -0400

    Added all JS and html

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ const content = document.getElementById("content")
 const control = document.getElementById("control")
 const message = document.getElementById("message")
 
-
-
-
-
 // Events 
 
 control.addEventListener("click", (event) => {
     //changes the state after startgame button is clicked
-    if(event.target.matches(".js-start")){
-        const word =  getWord()
+    if (event.target.matches(".js-start")) {
+        const word = getWord()
         state.play = true
         state.selectedWord = word
         state.displayWord = createBlanks(word)
@@ -21,19 +17,26 @@ control.addEventListener("click", (event) => {
         render()
         return;
     }
+    if (event.target.matches(".js-submit")) {
+        const getInput = document.getElementsByClassName("js-input")[0]
+        let input = getInput.value
+        checkLetters(input)
+
+
+    }
 })
 
 
 //Helpers
 
-const getWord = () =>{
+const getWord = () => {
     return state.words[Math.floor(Math.random() * state.words.length)];
 }
 
 const createBlanks = (word) => {
     const displayWord = []
-    for(let i=0; i < word.length; i++){
-         displayWord.push("_ ")
+    for (let i = 0; i < word.length; i++) {
+        displayWord.push("_ ")
     }
     return displayWord
 }
@@ -53,6 +56,24 @@ const renderControl = () => {
     <button class ="js-submit">Submit</button>`
 }
 
+const checkLetters = (input) => {
+    let doesNotExist = true
+    //check input against selectedWord
+    //if it does, replace dash with letter
+    for (let i = 0; i < state.selectedWord.length; i++) {
+        if (input === state.selectedWord[i]) {
+            state.displayWord[i] = state.selectedWord[i]
+            doesNotExist = false
+        }
+    }
+    if (doesNotExist) {
+        state.turns = state.turns - 1
+        state.guesses.push(input)
+    }
+    render()
+
+}
+
 
 //State
 const state = {
@@ -70,7 +91,7 @@ const state = {
 
 //Render
 const render = () => {
-    if(state.play === false){
+    if (state.play === false) {
         content.innerHTML = "<h2>Welcome to My Game</h2>"
         control.innerHTML = "<button class='js-start'>Start Game</button>"
         message.innerText = "Press start to begin your game"
@@ -80,6 +101,6 @@ const render = () => {
     control.innerHTML = renderControl()
     message.innerText = ""
     console.log(state);
-    
+
 }
 render()
